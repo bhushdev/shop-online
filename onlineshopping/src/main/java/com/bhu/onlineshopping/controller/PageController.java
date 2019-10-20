c48c82ba21043f3b8b34e71712512e8b6a1dd53c
Bhushan Powar <bhushanpowar9@gmail.com>
2019-10-21 01:06:46 +0530

    Completed interface,theme and other issues.

--- onlineshopping/src/main/java/com/bhu/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/bhu/onlineshopping/controller/PageController.java
@@ -11,7 +11,28 @@ public class PageController {
 	public ModelAndView index() {
 		
 		ModelAndView mv= new ModelAndView("page");
-		mv.addObject("greeting","Welcome to The Spring Web MVC.");
+		mv.addObject("title","HOME");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
+	
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about() {
+		
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title","About US");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact() {
+		
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title","Contact US");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	
 }
